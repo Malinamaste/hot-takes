c4ee6fdf0d33b46485a7ab17410f56ec3cb281bd
Melanie Morey <morey.melanie@gmail.com>
2023-02-22 14:20:53 +0100

    helmet update + clean

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 // Import des packages de sécurité
-//const cors = require('cors');
-//const helmet = require('helmet');
+const cors = require('cors');
+const helmet = require('helmet');
 
 // Import des routes sauces et user
-const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
 
 const app = express();
 
@@ -24,27 +24,11 @@ mongoose.connect('mongodb+srv://malinamaste:niokman4ever@cluster0.g6opnco.mongod
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-/************************************************************************
-                => CORS : Cross Origin Resource Sharing
-*************************************************************************/
-// Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de contrôle d'accès doivent être précisés pour tous nos objets de réponse.
-
-// Ces headers qui permettent...
-app.use((req, res, next) => {
-  // d'accéder à notre API depuis n'importe quelle origine ('*') :
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin, X-Requested-With, etc.) :
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  // d'envoyer des requêtes avec les méthodes mentionnées (GET, POST, etc.) :
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-
 // Cors
-//app.use(cors());
+app.use(cors());
 // Extra headers pour plus de sécurité -> routes
-//app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: false }));
+
 
 // Transforme les données des requêtes POST en objet JSON
 app.use(express.json());
--- routes/sauces.js
+++ routes/sauces.js
@@ -8,34 +8,23 @@ const multer = require('../middleware/multer-config');
 // On importe notre Controller Sauce :
 const saucesCtrl = require('../controllers/sauces');
 
-/******************************************************************
-          Récupération de la liste de toutes les sauces
-******************************************************************/
+
+// Récupération de la liste de toutes les sauces
 router.get('/', auth, saucesCtrl.getAllSauces);
 
-/******************************************************************
-        Enregistrement d'une sauce dans la base de données
-******************************************************************/
+// Enregistrement d'une sauce dans la base de données
 router.post('/', auth, multer, saucesCtrl.createSauce);
 
-/******************************************************
-          Récupération d'une sauce spécifique
-******************************************************/
+// Récupération d'une sauce spécifique
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 
-/******************************************************
-          Mettre à jour une sauce existante
-******************************************************/
+// Mettre à jour une sauce existante
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 
-/*************************************************
-          Liker ou Disliker une sauce
-*************************************************/
+// Liker ou Disliker une sauce
 router.post('/:id/like', auth, saucesCtrl.likesAndDislikes);
 
-/****************************************************
-                Suppression d'une sauce
-****************************************************/
+// Suppression d'une sauce
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
 
